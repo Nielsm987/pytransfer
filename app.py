9617cb053c32911aa0c129d582609e7bdc13ce16
Niels Moons <nielsmoons@MacBook-Air-van-Niels.local>
2023-11-25 11:42:03 +0100

    added shareable link

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 SERVICE_ACCOUNT_FILE = "service_account.json"
 PARENT_FOLDER_ID = "1K5Z9OwLBF-DhMjSLUAKSpgeWbtVPWJFY"
+
+file_name = input("Enter filename: ")
 
 
 def authenticate():
@@ -17,9 +20,23 @@ def upload(file_path):
     creds = authenticate()
     service = build("drive", "v3", credentials=creds)
 
-    file_metadata = {"name": "Hello", "parents": [PARENT_FOLDER_ID]}
+    file_metadata = {"name": file_name, "parents": [PARENT_FOLDER_ID]}
+    media = MediaFileUpload(file_path)
 
-    file = service.files().create(body=file_metadata, media_body=file_path).execute()
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+
+    # Get the file ID
+    file_id = file.get("id")
+
+    # Generate the share link
+    share_link = f"https://drive.google.com/file/d/{file_id}/view?usp=sharing"
+
+    return share_link
 
 
-upload("invoice.pdf")
+shareable_link = upload("invoice.pdf")
+print("Shareable link:", shareable_link)
