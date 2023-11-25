d87067a5fab6841cf1b215d717722d064d765e8f
Niels Moons <nielsmoons@MacBook-Air-van-Niels.local>
2023-11-25 12:45:40 +0100

    added command line arguments

--- pytransfer.py
+++ pytransfer.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import subprocess
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
@@ -5,8 +8,6 @@ from googleapiclient.http import MediaFileUpload
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 SERVICE_ACCOUNT_FILE = "service_account.json"
 PARENT_FOLDER_ID = "1K5Z9OwLBF-DhMjSLUAKSpgeWbtVPWJFY"
-
-file_name = input("Enter filename: ")
 
 
 def authenticate():
@@ -35,8 +36,27 @@ def upload(file_path):
     # Generate the share link
     share_link = f"https://drive.google.com/file/d/{file_id}/view?usp=sharing"
 
+    # Copy the shareable link to clipboard (macOS)
+    subprocess.run(["pbcopy"], text=True, input=share_link.strip())
+
     return share_link
 
 
-shareable_link = upload("invoice.pdf")
-print("Shareable link:", shareable_link)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_name>")
+    else:
+        file_name = sys.argv[1]
+        home_folder = os.path.expanduser("~")
+        found = False
+        for root, dirs, files in os.walk(home_folder):
+            if file_name in files:
+                found = True
+                file_path = os.path.join(root, file_name)
+                shareable_link = upload(file_path)
+                print("Shareable link:", shareable_link)
+                print("Shareable link copied to clipboard.")
+                break
+
+        if not found:
+            print("File not found in the home directory.")
